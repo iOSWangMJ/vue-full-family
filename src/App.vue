a858e6207572ade4ddcf616fced82208bf9b87ce
王铭杰 <wangmingjie@rmitec.cn>
2018-11-13 17:08:33 +0800

    新增proxy

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,33 @@
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  export default {
+    name: 'App',
+    mounted(){
+      this.$toast('Hello world!')
+      this.$http.post('http://localhost:3000/getMobileCode',{}).then(res=>{
+        console.log(res);
+      })
     }
   }
-}
+</script>
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import axios from 'axios'
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css'
+var moment = require('moment');
 
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response.data;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+Vue.prototype.$http=axios
 Vue.config.productionTip = false
+Vue.use(Mint)
+Vue.filter('datafmt',function (input,fmtstring) {
+  // 使用momentjs这个日期格式化类库实现日期的格式化功能
+  return moment(input).format(fmtstring);
+});
 
 new Vue({
   router,
